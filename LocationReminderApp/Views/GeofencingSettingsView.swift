13afe656b662eb001666224d791f864d07b1ad9b
ogaoga08 <kamekameshin0514@gmail.com>
2025-10-10 23:29:10 +0900

    ジオフェンス内外のバックグラウンド処理プロセス変更

--- LocationReminderApp/Views/GeofencingSettingsView.swift
+++ LocationReminderApp/Views/GeofencingSettingsView.swift
@@ -3,7 +3,7 @@ import MapKit
 import CoreLocation
 import Combine
 
-extension CLLocationCoordinate2D: Equatable {
+extension CLLocationCoordinate2D: @retroactive Equatable {
     public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
         return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
